2f1ff2732f48cca0ee03fe376048241a7967b58c
DgLsAlmeida <douglas.alu.lmb@gmail.com>
2022-10-19 11:36:18 -0300

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import { BackToTopButton } from "./components/BackToTopButton";
-import { Banner } from "./components/Banner";
-import { CompanySummary } from "./components/CompanySummary";
-import { CompanyValues } from "./components/CompanyValues";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import styles from "./styles/app.module.scss";
+import { Home } from "./pages/Home";
 import "./styles/global.scss";
 
 const App = () => {
-  return (
-    <div className={styles?.appContainer}>
-      <Header />
-      <CompanySummary />
-      <CompanyValues />
-      <Banner />
-      <Footer />
-      <BackToTopButton />
-    </div>
-  );
+  return <Home />;
 };
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import { BackToTopButton } from "../../components/BackToTopButton";
+import { Banner } from "../../components/Banner";
+import { CompanySummary } from "../../components/CompanySummary";
+import { CompanyValues } from "../../components/CompanyValues";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import styles from "./home.module.scss";
+
+export const Home = () => {
+  return (
+    <div className={styles?.homeContainer}>
+      <Header />
+      <CompanySummary />
+      <CompanyValues />
+      <Banner />
+      <Footer />
+      <BackToTopButton />
+    </div>
+  );
+};
